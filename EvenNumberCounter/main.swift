e620c48b6852845501d7840a98e3b7776b6ea3dc
Russell Gordon <rgordon@lcs.on.ca>
2024-10-11 13:14:06 -0400

    Solution with straight logic (no functions).

--- EvenNumberCounter/main.swift
+++ EvenNumberCounter/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+// Task: Count how many even numbers there are within a range
 
+// INPUT
+let lower = 1
+let upper = 15
+
+// PROCESS
+var countOfEvenNumbers = 0
+for i in lower...upper {
+    print(i)
+    
+    // Is this number even?
+    if i % 2 == 0 {
+        countOfEvenNumbers += 1
+        print("\(i) is even")
+    }
+}
+
+// OUTPUT
+print("Between \(lower) and \(upper) there are \(countOfEvenNumbers) even numbers.")
